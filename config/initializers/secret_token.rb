612491ada6713a3c29da1cec3760ba0f4cce7303
varun kumar <bit.varun@gmail.com>
2014-03-30 15:48:17 -0700

    Initial commit for my blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lazywiz::Application.config.secret_key_base = '916b4cccc7739e7291fb7da23653b34c0ea88bc3b93ca3a5e73ec09185dc4a557a4b3ce2e6f326a10e158d8f42d2287728039f7d234332819ae8de18eb11aac6'
